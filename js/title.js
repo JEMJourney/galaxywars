3c1226d42c5ac9a75ce4b700144fdc6af4e1649a
JEMJourney <jem.journey@gmail.com>
2016-04-25 22:10:33 +0200

    missing comma

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ InGame.prototype = {
 		game.add.sprite(0, 0, 'bg');
 
 		var status = game.add.text(game.world.centerX, game.world.centerY, 'Galaxy Wars', {
-			font: '64px ka1'
+			font: '64px ka1',
 			fill: '#ffffff'
 		});
 		status.anchor.set(0.5);
