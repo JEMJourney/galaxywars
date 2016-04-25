5a83b5afc915fa53400082615442d7f1494580e0
JEMJourney <jem.journey@gmail.com>
2016-04-25 22:07:44 +0200

    check font

--- js/title.js
+++ js/title.js
@@ -8,6 +8,7 @@ InGame.prototype = {
 		game.add.sprite(0, 0, 'bg');
 
 		var status = game.add.text(game.world.centerX, game.world.centerY, 'Galaxy Wars', {
+			font: '64px ka1'
 			fill: '#ffffff'
 		});
 
