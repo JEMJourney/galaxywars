29d0699cd14887a9d089599288da65550c3b91b1
JEMJourney <jem.journey@gmail.com>
2016-04-25 20:51:52 +0200

    physics on player

--- js/main.js
+++ js/main.js
@@ -44,6 +44,8 @@ function create() {
 
 	player = game.add.sprite(32, game.world.height - 150, 'player');
 
+	game.physics.arcade.enable(player);
+
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
