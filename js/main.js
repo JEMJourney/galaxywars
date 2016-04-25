513c8fc3c01673050032e54daeea34e254bc2473
JEMJourney <jem.journey@gmail.com>
2016-04-25 21:09:52 +0200

    faster star landing

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function create() {
 		
 		var star = stars.create(i * 70, 0, 'star');
 
-		star.body.gravity.y = 6;
+		star.body.gravity.y = 300;
 
 		star.body.bounce.y = 0.7 + Math.random() * 0.2;
 
