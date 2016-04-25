b435e10b0b923b3ee9b78a6a3bf21081be453c1f
JEMJourney <jem.journey@gmail.com>
2016-04-25 20:49:40 +0200

    adding player

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 var platforms;
+var player;
 
 function preload() {
 
 	game.load.image('star', 'assets/star.png');
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('ground', 'assets/ground.png');
+	game.load.image('player', 'assets/player.png');
 
 }
 
@@ -39,6 +41,12 @@ function create() {
 	ledge = platforms.create(-150, 250, 'ground');
 
 	ledge.body.immovable = true;
+
+	player = game.add.sprite(32, game.world.height - 15, 'player');
+
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
 
 }
 
